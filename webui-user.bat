0f22383e6302abc863d9720e3eba4ff071f5f321
tcy030412 <yajessica9980@gmail.com>
2023-10-05 21:52:03 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--medvram --opt-split-attention --enable-insecure-extension-access --xformers --listen
+
 
 call webui.bat
